16f43e696e964770f33bfb214854fa851a013ac0
Ayush-is-here <ayush18bhatt@gmail.com>
2026-09-02 14:11:10 +0530

    changesin cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -43,7 +43,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://healthcare-monitoring-ai-agent-copy.vercel.app.vercel.app",
+        "https://healthcare-monitoring-ai-agent-copy.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
